3282ee26dbf8b4bf807c392f9b0f80ce4ff062bc
AlienEdith <edith.w0807@gmail.com>
2020-03-14 16:40:13 -0700

    improvement on gravity is off

--- src/connect-k-python/StudentAI.py
+++ src/connect-k-python/StudentAI.py
@@ -175,6 +175,176 @@ class StudentAI():
         #print(list_return)
         return list_return
 
+    def get_heuristic_moves_gravity_off(self):
+        max_col = 0
+        max_row = 0
+        max_score = 0
+
+        for c in range(0,self.col):
+            for r in range(0,self.row):
+                curr_score = 0
+                
+                if(not self.objB.is_valid_move(c, r, True)):
+                    continue
+
+                # print("c: ", c, ", r:", r)
+                # Calculate left and right of this position and if they are valid
+                left = self.objB.is_valid_move(c-1, r, False)
+                right = self.objB.is_valid_move(c+1, r, False)
+                # print(left, right)
+
+                if(left and right):
+                    if(self.objB.get_my_board_val(c-1, r)==2 and self.objB.get_my_board_val(c+1, r)==2):
+                        curr_score += 4
+                        # print("left-right-1")
+                    if(self.objB.get_my_board_val(c-1, r)==1 and self.objB.get_my_board_val(c+1, r)==1):
+                        curr_score += 2
+                        # print("left-right-2")
+                    
+                if(left):
+                    #print("l1")
+                    #print(self.objB.get_my_board_val(c-1, r))
+                    if(self.objB.get_my_board_val(c-1, r) == 2):
+                        #print("l2")
+                        curr_score += 3
+                        # print("left-right-3")
+
+                    if(self.objB.get_my_board_val(c-1, r) == 1):
+                        #print("l2")
+                        curr_score += 1
+                        # print("left-right-4")
+
+                if(right):
+                    #print("r1")
+                    #print(self.objB.get_my_board_val(c+1, r))
+                    if(self.objB.get_my_board_val(c+1, r) == 2):
+                        #print("r2")
+                        curr_score += 3
+                        # print("left-right-5")
+
+                    if(self.objB.get_my_board_val(c+1, r) == 1):
+                        #print("r2")
+                        curr_score += 1
+                        # print("left-right-6")
+
+                top = self.objB.is_valid_move(c, r-1, False)
+                bottom = self.objB.is_valid_move(c, r+1, False)
+                if(top and bottom):
+                    # print("tb")
+                    if(self.objB.get_my_board_val(c, r-1)==2 and self.objB.get_my_board_val(c, r+1)==2):
+                        curr_score += 4
+                        # print("top-bottom-1")
+
+                    if(self.objB.get_my_board_val(c, r-1)==1 and self.objB.get_my_board_val(c, r+1)==1):
+                        curr_score += 2
+                        # print("top-bottom-2")
+
+
+                if(bottom):
+                    # print("bottom")
+                    #print(self.objB.get_my_board_val(c-1, r))
+                    if(self.objB.get_my_board_val(c, r+1) == 2):
+                        #print("l2")
+                        curr_score += 3
+                        # print("top-bottom-3")
+
+                    if(self.objB.get_my_board_val(c, r+1) == 1):
+                        #print("l2")
+                        curr_score += 1
+                        # print("top-bottom-4")
+                
+                if(top):
+                    #print("r1")
+                    #print(self.objB.get_my_board_val(c+1, r))
+                    if(self.objB.get_my_board_val(c, r-1) == 2):
+                        #print("r2")
+                        curr_score += 3
+                        # print("top-bottom-5")
+
+                    if(self.objB.get_my_board_val(c, r-1) == 1):
+                        #print("r2")
+                        curr_score += 1
+                        # print("top-bottom-6")
+
+                # Left-Cell-Right
+                # Calculate top-left and bottom-right of this position and if they are valid
+                top_left = False
+                bottom_right = False
+
+                if(r+self.col-c>=self.k):
+                    top_left = self.objB.is_valid_move(c-1, r-1, False)
+
+                    if(top_left and self.objB.get_my_board_val(c-1, r-1) == 2):
+                        curr_score += 3
+                        # print("topleft-bottomright-1")
+
+                    if(top_left and self.objB.get_my_board_val(c-1, r-1) == 1):
+                        curr_score += 1
+                        # print("topleft-bottomright-2")
+
+                    bottom_right = self.objB.is_valid_move(c+1, r+1, False)
+
+                    if(bottom_right and self.objB.get_my_board_val(c+1, r+1) == 2):
+                        curr_score += 3
+                        # print("topleft-bottomright-3")
+
+                    if(bottom_right and self.objB.get_my_board_val(c+1, r+1) == 1):
+                        curr_score += 1
+                        # print("topleft-bottomright-4")
+
+                # Both
+                if(top_left and bottom_right):
+                    if(self.objB.get_my_board_val(c-1, r-1)==2 and self.objB.get_my_board_val(c+1, r+1)==2):
+                        curr_score += 4
+                        # print("topleft-bottomright-5")
+
+                    if(self.objB.get_my_board_val(c-1, r-1)==1 and self.objB.get_my_board_val(c+1, r+1)==1):
+                        curr_score += 2
+                        # print("topleft-bottomright-6")
+
+
+                # Calculate top-right and bottom-left of this position and if they are valid
+                top_right = False
+                bottom_left = False
+
+                if(c+r >= self.k-1):
+                    top_right = self.objB.is_valid_move(c+1, r-1, False)
+
+                    if(top_right and self.objB.get_my_board_val(c+1, r-1) == 2):
+                        curr_score += 3
+                        # print("topright-bottomleft-1")
+
+                    if(top_right and self.objB.get_my_board_val(c+1, r-1) == 1):
+                        curr_score += 1
+                        # print("topright-bottomleft-2")
+
+                    bottom_left = self.objB.is_valid_move(c-1, r+1, False)
+
+                    if(bottom_left and self.objB.get_my_board_val(c-1, r+1) == 2):
+                        curr_score += 3
+                        # print("topright-bottomleft-3")
+
+                    if(bottom_left and self.objB.get_my_board_val(c-1, r+1) == 1):
+                        curr_score += 1
+                        # print("topright-bottomleft-4")
+
+                if(top_right and bottom_left):
+                    if(self.objB.get_my_board_val(c+1, r-1)==2 and self.objB.get_my_board_val(c-1, r+1)==2):
+                        curr_score += 4
+                        # print("topright-bottomleft-5")
+
+                    if(self.objB.get_my_board_val(c+1, r-1)==1 and self.objB.get_my_board_val(c-1, r+1)==1):
+                         curr_score += 2
+                        #  print("topright-bottomleft-6")
+
+                # print("current score: ", curr_score)
+                if(curr_score > max_score):
+                    max_score = curr_score
+                    max_col = c
+                    max_row = r
+
+            #print(list_return)
+        return max_col, max_row
 
     def add_adj_cells(self, move):
         max_score = 0
@@ -195,7 +365,10 @@ class StudentAI():
                     if(not self.objB.is_valid_move(tmpCol, tmpRow, False)):     
                         continue
 
-                    if(self.objB.get_my_board_val(tmpCol, tmpRow) == 1 or self.objB.get_my_board_val(tmpCol, tmpRow) == 2):
+                    if(self.objB.get_my_board_val(tmpCol, tmpRow) == 1):
+                        score += 2
+
+                    if(self.objB.get_my_board_val(tmpCol, tmpRow) == 2):
                         score += 1
 
                 if(score > max_score):
@@ -219,8 +392,10 @@ class StudentAI():
                 self.objB.make_my_move(move.col, move.row, 2)
 
                 # add all adjecent cells which are empty to possible moves to choose from
-                newCol, newRow = self.add_adj_cells(move)
-            
+                # newCol, newRow = self.add_adj_cells(move)
+
+                newCol, newRow = self.get_heuristic_moves_gravity_off()
+                print("newCol: ", newCol, ", newRow: ", newRow)
                 #self.objB.my_show_board();        
             
                 # check around this move if it makes it win for 2
